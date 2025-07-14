917f2122de49b96219cb4fb4cfc9751184d62c52
aidar <aidar.sharaev@yandex.ru>
2025-07-14 21:41:38 +0300

    fixed detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.analyzer.detekt) apply false
+    alias(libs.plugins.analyzer.kover) apply false
 }
